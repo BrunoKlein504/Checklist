b98690d48bc26ad2e09744b6473c87c35f9e5768
Bruno Klein Gomes <bruno.gomes@deepessoas.com.br>
2026-06-11 17:59:31 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,91 @@
+from utils import *
+
+df = read_excel()
+
+st.set_page_config(layout="wide")
+
+st.title("Checklist - Abril")
+
+
+df.drop(df.loc[:, "Acumulado Prev":"Previsão mês próximo"], axis=1, inplace=True)
+df.drop("Unnamed: 31", axis=1, inplace=True)
+df.drop(df.query("Pilar == '-' or Pilar == 'TOTAL ACUMULADO' or Distribuidora == 'Global Distribuidora'").index,inplace=True)
+nova_col_dict = {col: "Realizado {}/26".format(name_Month(col)) for col in df.loc[:, "2026-01-01 00:00:00.1":].columns}
+df.rename(columns=nova_col_dict, inplace=True)
+nova_icol_dict = {col: "Previsto {}/26".format(col.strftime("%B").capitalize()) for col in df.iloc[:, 19:31].columns}
+df.rename(columns=nova_icol_dict, inplace=True)
+df_global = df.query("Regional == 'TODAS'")
+df_regionais = df.query("Regional != 'TODAS'")
+
+
+Distribuidoras = list(df_global['Distribuidora'].unique())
+
+mask = df.columns.str.contains((datetime.now() - relativedelta(months=1)).strftime("%B").capitalize())
+matching_columns = df.columns[mask]
+
+tabelas_distribuidoras = {}
+
+for distribuidora in Distribuidoras:
+    
+    tabela_filtrada = df_global.query(
+        "`Status das ações` == 'Iniciada' and "
+        "Distribuidora == @distribuidora and "
+        "Pilar != 'CAPACITAÇÃO' and "
+        "`Ação Iniciada?` == 'Em Execução na Distribuidora'"
+    ).groupby("Pilar").agg(
+        Apuração=("Ação Iniciada?", "count"), 
+        Recebido=(matching_columns[1], "count") 
+    ).assign(
+        Status=lambda x: x['Recebido'] / x['Apuração'] 
+    )
+    
+    tabelas_distribuidoras[distribuidora] = tabela_filtrada
+
+
+for name, tab in tabelas_distribuidoras.items():
+    st.subheader(name)
+    
+
+    tab_corrigida = tab.reset_index()
+    
+
+    ordem_desejada = ["COMPORTAMENTO", "POPULAÇÃO", "FORNECEDOR", "LIDERANÇA"]
+    
+   
+    tab_corrigida['Pilar'] = pd.Categorical(
+        tab_corrigida['Pilar'], 
+        categories=ordem_desejada, 
+        ordered=True
+    )
+    
+    tab_corrigida = tab_corrigida.sort_values('Pilar')
+    
+
+    df_estilizado = (
+        tab_corrigida.style
+        .map(colorir_pilar, subset=['Pilar']) 
+        .map(colorir_status, subset=['Status']) 
+        .format({'Status': '{:.0%}'})
+    )
+    
+    st.dataframe(df_estilizado, hide_index=True, use_container_width=True, column_config={
+        "Apuração":"Qntd de Apurações",
+        "Recebido":"Qntd de Recebidos",
+        "Status":"Status de Recebimento"
+    })
+
+    st.divider()
+
+
+
+# st.markdown(
+#     '<iframe title="Jornada de Segurança - Dash Distribuidoras 2026" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYjZkZGJiODEtMmExMS00ZGRjLWE0YzQtOTE4ZGZhNGU2ZTJlIiwidCI6IjkxZDEwNWNkLTEwYzYtNDJkMC04N2VlLWFjMDg2YmM1YTUyNyJ9&pageName=ReportSection9396034a29bd106109e9" frameborder="0" allowFullScreen="true"></iframe>',
+#     unsafe_allow_html=True
+# )
+
+# st.iframe("https://app.powerbi.com/view?r=eyJrIjoiYjZkZGJiODEtMmExMS00ZGRjLWE0YzQtOTE4ZGZhNGU2ZTJlIiwidCI6IjkxZDEwNWNkLTEwYzYtNDJkMC04N2VlLWFjMDg2YmM1YTUyNyJ9&pageName=ReportSection9396034a29bd106109e9")
+
+st.markdown(
+    '<iframe title="Jornada de Segurança - Dash Distribuidoras 2026" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYjZkZGJiODEtMmExMS00ZGRjLWE0YzQtOTE4ZGZhNGU2ZTJlIiwidCI6IjkxZDEwNWNkLTEwYzYtNDJkMC04N2VlLWFjMDg2YmM1YTUyNyJ9" frameborder="0" allowFullScreen="true"></iframe>',
+    unsafe_allow_html=True
+)
---
+++ utils.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import locale
+from dateutil.relativedelta import relativedelta
+
+import math
+
+@st.cache_data
+def read_excel():
+    df = pd.read_excel(r"Bases de Indicadores - Jornada de Segurança 2026 - Todas.xlsx", sheet_name="Previsto x realizado")
+    return df
+
+def name_Month(re_month:object) -> str:
+    from re import match
+
+    try:
+        locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+    except locale.Error:
+        print("⚠️ Locale pt_BR.UTF-8 não disponível no sistema. Usando formato numérico.")
+
+    match_month:object = match(r"\d{4}-\d{2}-\d{2}", re_month)[0]
+
+    month_raw:datetime = datetime(*list(map(int,match_month.split('-'))))
+
+    month_str:str = month_raw.strftime("%B")
+
+    return month_str.capitalize()
+
+def colorir_pilar(valor):
+    
+    cores = {
+        "COMPORTAMENTO": "background-color: #FD8C03; color: white;", # Laranja
+        "FORNECEDOR": "background-color: #7030A0; color: white;",    # Roxo
+        "LIDERANÇA": "background-color: #4472C4; color: white;",     # Azul
+        "POPULAÇÃO": "background-color: #5B9BD5; color: white;"      # Azul Claro
+    }
+    
+    return cores.get(valor, '')
+
+
+def colorir_status(valor):
+    
+    if valor >= 1.0:
+        cor = '#c8e6c9' # Verde (Atingiu a meta)
+    elif valor >= 0.5:
+        cor = '#fff9c4' # Amarelo (Atenção)
+    else:
+        cor = '#ffcdd2' # Vermelho (Abaixo do esperado)
+        
+    return f'background-color: {cor}; color: #000000;'
